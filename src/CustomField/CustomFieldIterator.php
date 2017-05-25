88cd3517eae2799ee57199be510d4e81d54377d1
Artur Sh. Mamedbekov <Artur-Mamedbekov@yandex.ru>
2017-05-25 17:20:17 +0300

    Fix

--- src/CustomField/CustomFieldIterator.php
+++ src/CustomField/CustomFieldIterator.php
@@ -6,8 +6,10 @@ namespace AmoCRMAPI\CustomField;
  */
 class CustomFieldIterator extends \ArrayIterator{
   public function fetchFromName($name){
-    return array_pop(array_filter((array) $this, function($customField) use($name){
+    $customField = array_filter((array) $this, function($customField) use($name){
       return $customField->getName() == $name;
-    }));
+    });
+
+    return array_pop($customField);
   }
 }
